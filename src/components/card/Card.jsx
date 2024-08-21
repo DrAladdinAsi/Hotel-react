cf124b350b48d5dce87d1c9bd0fe9304374cc788
AladdinAsi <76973081+DrAladdinAsi@users.noreply.github.com>
2024-08-21 10:17:17 +0300

    Update Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import './card.scss'
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 function Card({item}) {
+
+
+  const navigate = useNavigate();
+
+  const handelCardClicked = (id)=>{
+       navigate(`hotel/${id}`);
+  }
+
   return (
 
     <div className='card_Hotel'>
     <Link to={`hotel/${item.id}`} className='the_image_container_card'>
       <img src={`https://django-ten-sable.vercel.app${item.H_photo}`} alt="the image for the card" />
     </Link>
-    <div className="text_container_card">
+    <div className="text_container_card" onClick={()=>{handelCardClicked(item.id)}}>
       <h2 className="title_for_card">{item.name}</h2>
       <p className="location_sec">
         <img src="/images/pin.png" alt="the location image" />
